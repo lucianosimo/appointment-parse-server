51e3e8133c304662b55058307c980968ede465c2
Luciano Simo <lucianosimo87@gmail.com>
2017-06-23 15:57:35 -0300

    Added survey link

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post('/surveyEmail', function(req,res) {
       html: "<div style='margin-bottom:15px'><img src='https://www.oneclickstore.com/oneonone/mail/mail-logo.png'></div>" +
             "<hr style='display:block;height:2px;background-color:#cb3630;margin-bottom:25px;border:none'>" +
             "<div style='text-align:center;font-size:28px;'><strong>" + req.body.mailBody + "</strong></div>" +
-            "<div style='text-align:center;font-size:28px;'>" + surveyLink + "</div>" +
+            "<div style='text-align:center;font-size:28px;'><a href=" + surveyLink + "></div>" +
             "<hr style='display:block;height:2px;background-color:#cb3630;margin-top:25px;border:none'>"
     }
 
